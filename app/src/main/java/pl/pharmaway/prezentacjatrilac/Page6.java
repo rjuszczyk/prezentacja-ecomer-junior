75a81172ee0a81d27db0196031cc1683f3117106
Radosław Juszczyk <radoslaw.juszczyk@tigerspike.com>
2018-02-12 18:33:58 +0400

    navigation back from summary

--- app/src/main/java/pl/pharmaway/prezentacjatrilac/Page6.java
+++ app/src/main/java/pl/pharmaway/prezentacjatrilac/Page6.java
@@ -1,6 +1,29 @@
 package pl.pharmaway.prezentacjatrilac;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 public class Page6 extends FooterActivity {
+
+    boolean goBackToMenu;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        goBackToMenu = getIntent().getBooleanExtra("goBackToMenu", false);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(!goBackToMenu) {
+            super.onBackPressed();
+        } else {
+            Intent intent = new Intent(this, Page2.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        }
+    }
 
     @Override
     protected int getLayoutResourceId() {
